c43defe825eff22a8671fb7cd2c412f8e7a14f2e
Barış <98894218+Baris007@users.noreply.github.com>
2024-10-23 18:55:45 +0300

    güncel

--- API/Farm2Market.API/Program.cs
+++ API/Farm2Market.API/Program.cs
@@ -1,10 +1,12 @@
 using Farm2Market.Domain;
 using Farm2Market.Infrastructure.Data;
 using Farm2Market.Infrastructure.Repository;
+using Farm2Marrket.Application.DTOs;
 using Farm2Marrket.Application.Manager;
 using Farm2Marrket.Application.Sevices;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,14 +15,36 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-
-
+builder.Services.AddSwaggerGen(c =>
+{
+	c.SwaggerDoc("v1", new OpenApiInfo
+	{
+		Title = "Example API",
+		Version = "v1",
+		Description = "An example of an ASP.NET Core Web API",
+		Contact = new OpenApiContact
+		{
+			Name = "Example Contact",
+			Email = "example@example.com",
+			Url = new Uri("https://example.com/contact"),
+		},
+	});
+}); ;
 builder.Services.AddDbContext<AppDbContext>(options =>
-           options.UseMySql("server=localhost;port=3306;user=root;password=root;database=farm2markett", new MySqlServerVersion(new Version(8, 0, 40))));
+           options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"),
+    new MySqlServerVersion(new Version(8, 0, 40))));
+builder.
+    Services.AddIdentity<AppUser, IdentityRole>()
+            .AddEntityFrameworkStores<AppDbContext>()
+            .AddDefaultTokenProviders();
 builder.Services.AddScoped<IUserService, UserManager>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.Configure<MailSettings>(builder.Configuration.GetSection("SmtpSettings"));
+builder.Services.AddTransient<IEmailService, EmailService>();
+
+builder.Services.AddAuthorization();
+
+
 var app = builder.Build();
 
 
@@ -29,7 +53,11 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+	app.UseSwaggerUI(options =>
+	{
+		options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+	});
+
 }
 
 app.UseHttpsRedirection();
